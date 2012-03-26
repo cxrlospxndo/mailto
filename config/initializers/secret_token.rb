fa2a2faa42b3f310f8a9a1f82d7ace0b169b07e4
cxrlospxndo <cxrlospxndo@gmail.com>
2012-03-25 22:07:27 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailit::Application.config.secret_token = 'fc6a8f5ffff6d766fc6c2c07c40c8af0dbf967e2a50435015b4696fbc94c19b31215bd6a7d48d859118b4bbccf27f0ef876338bd0bb0396a3faa70d2722f3ea2'
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "http://localhost:3000",
+  :user_name            => "cxrlospxndo",
+  :password             => "B8kxVB8kxV",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
